60247096fb67ca10bbc806d1d09f3487a0fe05bd
Rohit Phalke <rohitphalke165@gmail.com>
2024-05-24 01:03:15 +0530

    done some work on chatbox

--- src/component/Chatbox.js
+++ src/component/Chatbox.js
@@ -2,24 +2,25 @@ import React from 'react'
 import StarBorderIcon from '@mui/icons-material/StarBorder';
 import InfoIcon from '@mui/icons-material/Info';
 import Chatinput from './Chatinput';
+import { grey } from '@mui/material/colors';
 
 function Chatbox() {
   return (
-    <div className=' w-full bg-red-400 h-screen  '>
-      <div className='flex justify-between items-center px-2 py-2'>
-        <header className='flex items-center'>
+    <div className=' w-4/5 bg-gray-200 h-screen  '>
+      <div className=' w-full flex justify-between items-center px-2 border-b-2 border-gray-400 py-2'>
+        <div className='flex items-center'>
           <h4 className='font-bold mr-2'>#room-name</h4>
           <StarBorderIcon className='cursor-pointer' sx={{fontSize:20}}/>
-        </header>
-        <header className='flex items-center'>
-          <InfoIcon/>
-          <h4 className='ml-1'>Details</h4>
-        </header>
-        <div>
+        </div>
+        <div className='flex items-center '>
+          <InfoIcon sx={{color:grey[600] , fontSize:20}}/>
+          <h4 className='ml-1 text-sm'>Details</h4>
+        </div>
+      </div>
+      <div>
           {/* chat messages */}
         </div>
         <Chatinput/>
-      </div>
     </div>
   )
 }
