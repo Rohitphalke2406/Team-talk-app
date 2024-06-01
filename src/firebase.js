a9985a4cb4a383e19a188a0d466cf3c0d79d30c8
Rohit Phalke <rohitphalke165@gmail.com>
2024-06-01 05:47:16 +0530

    changes in files

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCUgDXQ480syx0L_THgpnXfpJZSeJrTYNE",
+  authDomain: "slack-clone-766cc.firebaseapp.com",
+  projectId: "slack-clone-766cc",
+  storageBucket: "slack-clone-766cc.appspot.com",
+  messagingSenderId: "603632335587",
+  appId: "1:603632335587:web:4898e8cb237e0bf03c63d6",
+  measurementId: "G-5FDR1JELC5"
 };
 
 // Initialize Firebase
